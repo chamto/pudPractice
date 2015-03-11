1cf893b2e8aa970f040b871eb7fed6c41f383362
chamto <anrudco@gmail.com>
2015-03-11 23:43:01 +0900

    [ 육각형타일맵을 시험하기 위한 씬 추가 ] ExampleMesh : 선을 그리기 위해, 메쉬 실시간 수정 어떻게 하는지 시험해본 클래스 TestHexmap : 1차적으로 육각형타일맵을 그릴려고함 , 선그리기 함수 제작중

---
+++ Assets/Test/ExampleMesh.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleMesh : MonoBehaviour {
+
+
+	public Vector3[] newVertices;
+	public Vector2[] newUV;
+	public int[] newTriangles;
+
+	void Start2() 
+	{
+		Mesh mesh = new Mesh();
+		GetComponent<MeshFilter>().mesh = mesh;
+		mesh.vertices = newVertices;
+		mesh.uv = newUV;
+		mesh.triangles = newTriangles;
+
+	}
+
+
+	void Update() 
+	{
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		Vector3[] vertices = mesh.vertices;
+		Vector3[] normals = mesh.normals;
+		//Debug.Log(vertices.Length); //chamto test
+		int i = 0;
+		while (i < vertices.Length) {
+			vertices[i] += normals[i] * Mathf.Sin(Time.time);
+			//Debug.Log(vertices[i]); //chamto test
+			i++;
+		}
+		mesh.vertices = vertices;
+	}
+
+
+}
---
+++ Assets/Test/TestHexmap.cs
@@ -0,0 +1,150 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class TestHexmap : MonoBehaviour 
+{
+
+//ref : http://gamedev.stackexchange.com/questions/20742/how-can-i-implement-hexagonal-tilemap-picking-in-xna
+//
+//	             Y ^
+//		           |
+//			   ____|____
+//			  /  b |   |\
+//			 /     |   | \
+//			/      |   |  \
+//	    ---(-------+---+---)------>
+//			\     O|   c  / a      X
+//			 \     |     /
+//			  \____|____/
+//			       |
+
+//	      /|
+//     a / |
+//	    /  | b
+//     /   |
+//    (----ㅁ
+//	     c
+
+	//"a" is the radius of circumscribed that hexagon
+	//"b" is the radius of inscribed that hexagon
+	//"c" is the base or the top edge
+	public float hexCircumScr;	// "a"
+	public float hexInScr;		// "b"
+	public float hexBase;		// "c"
+
+
+	// Use this for initialization
+	void Start1 () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update1 () 
+	{
+	
+	}
+
+
+	/// <summary>
+	/// test line render
+	/// ref : http://www.andrewnoske.com/wiki/Unity_-_Mesh
+	/// </summary>
+	private Mesh mesh;
+	private Material mat;
+
+	public Vector3 m_linePos;
+	
+	private Vector3 linePos
+	{
+		get
+		{
+			return m_linePos;
+		}
+
+		set 
+		{
+
+			if(null != mesh.vertices)
+			{
+				Vector3[] vertices = mesh.vertices;
+				for(int i=0;i < mesh.vertexCount; i++)
+				{
+					//mesh.vertices.SetValue(Vector3.zero,i);
+					//mesh.vertices[i] += value;
+					vertices[i] += value;
+					Debug.Log ("mesh.vertices "+i+": "+ mesh.vertices[i]);
+				}
+				mesh.vertices = vertices;
+				
+			}
+		}
+	}
+
+	
+	void Start ()
+	{
+		mat = new Material(Shader.Find("Self-Illumin/VertexLit"));
+		mat.color = new Color(0.6f,0.3f,0);
+		
+		mesh = new Mesh();
+		
+		Vector3[] newVertices  = new Vector3[4];  // Setup vertices.
+		newVertices[0] = new Vector3(0,0,0);
+		newVertices[1] = new Vector3(0,5,0);
+		newVertices[2] = new Vector3(5,5,0);
+		newVertices[3] = new Vector3(5,0,0);
+
+		//newVertices[3] = new Vector3(0,0,0);
+		//newVertices[4] = new Vector3(5,5,0);
+		//newVertices[5] = new Vector3(5,0,0);
+		
+		int[] newTriangles = new int[6];      // Map these 3 verticies into one triangle.
+		newTriangles[0] = 0;
+		newTriangles[1] = 1;
+		newTriangles[2] = 2;
+
+		newTriangles[3] = 0;
+		newTriangles[4] = 2;
+		newTriangles[5] = 3;
+		
+//		newTriangles[3] = 3;
+//		newTriangles[4] = 4;
+//		newTriangles[5] = 5;
+		
+		mesh.vertices = newVertices;
+
+		mesh.triangles = newTriangles;
+		//mesh.uv       = newTextureCoord;  // For texture mapping using a Vector2[].
+		//mesh.normals  = newNormal;        // For configuring normals using a Vector3[].
+		//mesh.colors   = newColorVert;     // Vary color by vertex using a Color[].
+		mesh.RecalculateBounds();
+		mesh.RecalculateNormals();
+
+	}
+	
+	
+	void Update()
+	{
+
+
+		Graphics.DrawMesh(mesh, transform.localToWorldMatrix, mat, 0);
+	}
+
+	#if UNITY_EDITOR
+	void OnGUI()
+	{
+		if (GUI.Button (new Rect (10, 10, 100, 70), "update")) 
+		{
+			linePos = m_linePos;
+			mesh.RecalculateBounds();
+			mesh.RecalculateNormals();
+
+			Debug.Log ("pos update!!!");
+		}
+		
+
+	}
+	#endif
+}
