6fb7da95b3ffb58fd54f56492aac148d03c65ad7
chamto <anrudco@gmail.com>
2015-03-14 00:39:18 +0900

    [클릭한 곳의 헥스맵좌표 얻어오기 함수 추가] -GetHex , PointToHex  이 두함수는 핵스맵에 대한 y축 좌표 체계가 다르다.  헥스맵좌표는 성공적으로 반환한다 * 다음할일 : 두 함수의 알고리즘에 대한 분석이 필요함

--- Assets/Test/TestHexmap.cs
+++ Assets/Test/TestHexmap.cs
@@ -72,8 +72,91 @@ public class TestHexmap : MonoBehaviour
 		// Update is called once per frame
 		void Update ()
 		{
-	
+
+			if(Input.GetMouseButtonUp(0))
+			//if (Input.touchCount > 0) 
+			{
+				//if (Input.GetTouch (0).phase == TouchPhase.Ended) 
+				{
+					CallBackTouchEnded();
+				}
+			}
 		}
+
+		void CallBackTouchEnded()
+		{
+			//Vector3 pos =  Camera.main.ScreenToWorldPoint (Input.GetTouch(0).position);
+			Vector3 pos =  Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			
+			int HexPosX = 0, HexPosY = 0;
+			
+			
+			//GetHex (pos.x, pos.y, out HexPosY, out HexPosX);
+			PointToHex (pos.x, pos.y, out HexPosX, out HexPosY);
+
+			Debug.Log ("WorldPos : " + pos + "  HexPos : " + HexPosX + " " + HexPosY);
+		}
+
+		//ref : http://gamedev.stackexchange.com/questions/20742/how-can-i-implement-hexagonal-tilemap-picking-in-xna
+		void GetHex(float x, float y, out int row, out int column)
+		{
+			float a = hexCircumScr;
+			float b = hexInScr;
+			float c = hexBase;
+
+			// Find out which major row and column we are on:
+			row = (int)(y / b);
+			column = (int)(x / (a + c));
+			
+			// Compute the offset into these row and column:
+			float dy = y - (float)row * b;
+			float dx = x - (float)column * (a + c);
+			
+			// Are we on the left of the hexagon edge, or on the right?
+			if (((row ^ column) & 1) == 0)
+				dy = b - dy;
+			int right = dy * (a - c) < b * (dx - c) ? 1 : 0;
+			
+			// Now we have all the information we need, just fine-tune row and column.
+			row += (column ^ row ^ right) & 1;
+			column += right;
+		}
+
+		//ref : http://www-cs-students.stanford.edu/~amitp/Articles/GridToHex.html
+		void PointToHex( float xp, float yp ,out int hexX , out int hexY)
+		{
+
+			// NOTE:  HexCoord(0,0)'s x() and y() just define the origin
+			//        for the coordinate system; replace with your own
+			//        constants.  (HexCoord(0,0) is the origin in the hex
+			//        coordinate system, but it may be offset in the x/y
+			//        system; that's why I subtract.)
+			float x = xp / (hexCircumScr + hexBase);
+			float y =  yp / (hexInScr *2);
+			float z = -0.5f * x - y;
+			y = -0.5f * x + y;
+			int ix = Mathf.FloorToInt(x+0.5f);
+			int iy = Mathf.FloorToInt(y+0.5f);
+			int iz = Mathf.FloorToInt(z+0.5f);
+			int s = ix+iy+iz;
+			if( 0 != s )
+			{
+				float abs_dx = Mathf.Abs(ix-x);
+				float abs_dy = Mathf.Abs(iy-y);
+				float abs_dz = Mathf.Abs(iz-z);
+				if( abs_dx >= abs_dy && abs_dx >= abs_dz )
+					ix -= s;
+				else if( abs_dy >= abs_dx && abs_dy >= abs_dz )
+					iy -= s;
+				else
+					iz -= s;
+			}
+			hexX = ix;
+			hexY = (iy - iz + (1-ix%2) ) / 2;
+			//return HexCoord( ix, ( iy - iz + (1-ix%2) ) / 2 );
+		}
+	
+
 
 		int AddHexagon (Vector3 setPos)
 		{
@@ -88,7 +171,7 @@ public class TestHexmap : MonoBehaviour
 
 			lineRenderer.material = new Material (Shader.Find ("Particles/Additive"));
 			lineRenderer.SetColors (Color.blue, Color.blue);
-			lineRenderer.SetWidth (0.2F, 0.2F);
+			lineRenderer.SetWidth (0.1F, 0.1F);
 			lineRenderer.SetVertexCount (7);
 
 			pos.x = -hexBase; pos.y = hexInScr; pos.z = 0;
@@ -126,7 +209,6 @@ public class TestHexmap : MonoBehaviour
 
 			hexHypotenuse = Mathf.Sqrt (hexBase * hexBase + hexInScr * hexInScr);
 			
-
 
 			Debug.Log ("Add hexagon that Id : " + AddHexagon (setPosHexagon));
 		}
