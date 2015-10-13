f84c4e04a1c8adf1f71f67d17701fb966c34e003
chamto <anrudco@gmail.com>
2015-10-13 23:28:28 +0900

    [ MainUI 이벤트 스크립트 파일 추가 ] 버튼과 이벤트 함수 연결 시

---
+++ Assets/Script/3_UI/MainUI_Script.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainUI_Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Callback_Btn_InfinitePractice()
+	{
+		
+		Debug.Log ("--------Callback_Btn_InfinitePractice 1 ~ ");
+	}
+
+	public void Callback_Btn_PattenExtraction()
+	{
+		
+		Debug.Log ("--------Callback_Btn_PattenExtraction 2 ~ ");
+	}
+
+	public void Callback_Btn_PattenPractice()
+	{
+		
+		Debug.Log ("--------Callback_Btn_PattenPractice 3 ~ ");
+	}
+}
